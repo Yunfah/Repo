503889e4e012e74576449bffbac1a486ad630d92
Elina Kock <elina.kock@gmail.com>
2018-03-06 11:37:37 +0100

    Testing continues

---
+++ src/yunTest/Bla.java
@@ -0,0 +1,9 @@
+package yunTest;
+
+public class Bla {
+	public static void main(String[] args) {
+		for (int i = 0; i < 25; i++)
+			System.out.println("Yamma poopyhed " + i);
+	}
+
+}
--- src/yunTest/Test3.java
+++ src/yunTest/Test3.java
@@ -1,7 +1,8 @@
 package yunTest;
 
-public class Testning_2 {	
+public class Test3 {
 	public static void main (String [] args) {
-		System.out.print("Hello World");
+		System.out.print("Tast3");
 	}
+
 }
