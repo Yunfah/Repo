d4fb78d27a9a46d5f28a1c9fa8ec8b02b2ca8058
Yamma <yamma.sarwari@gmail.com>
2018-03-19 11:48:25 +0100

    test

--- src/client/ViewerGame.java
+++ src/client/ViewerGame.java
@@ -1,6 +1,10 @@
 package client;
 import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ViewerGame extends JPanel {
+    private List<JButton> letterButtons = new ArrayList<>();
+
 
 }
