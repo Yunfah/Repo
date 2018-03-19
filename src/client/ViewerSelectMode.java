771a9f62e281dc44e4e3214790903a3be941878f
Yun <yunfah.chow@gmail.com>
2018-03-19 12:03:06 +0100

    Fixed the VSM

--- src/client/ViewerSelectMode.java
+++ src/client/ViewerSelectMode.java
@@ -7,31 +7,20 @@ import javax.swing.*;
 
 public class ViewerSelectMode extends JPanel {
 	private JPanel pnlButtons = new JPanel();
-	private JLabel lblHeader = new JLabel("Hangman    ", SwingConstants.CENTER);
-	private JButton btnBack = new JButton("<-- BACK");
+	private JLabel lblHeader = new JLabel("Hangman", SwingConstants.CENTER);
+
 	private JButton btnSingle = new JButton("Singleplayer");
 	private JButton btnMulti = new JButton(" Multiplayer ");
+
 	
 	public ViewerSelectMode() {
 		setPreferredSize(new Dimension (1200,800));
 		setLayout(new BorderLayout());
 		setBackground(Color.DARK_GRAY);
 		
-		btnSingle.setFont(new Font("Sans Serif", Font.PLAIN, 40));
-		btnMulti.setFont(new Font("Sans Serif", Font.PLAIN, 40));
-		
-		add(pnlButtons, BorderLayout.CENTER);
-		
-		pnlButtons.setBackground(Color.DARK_GRAY);
-		pnlButtons.setLayout(new BoxLayout(pnlButtons, BoxLayout.Y_AXIS));
-		btnSingle.setAlignmentX(Component.CENTER_ALIGNMENT);
-		pnlButtons.add(Box.createRigidArea(new Dimension(100,150)));
-		btnMulti.setAlignmentX(Component.CENTER_ALIGNMENT);
-		
-		
 		add(titlePanel(), BorderLayout.NORTH);
-		pnlButtons.add(btnSingle);
-		pnlButtons.add(btnMulti);
+		add(buttonPanel(), BorderLayout.CENTER);
+		
 		
 	}
 	
@@ -39,20 +28,28 @@ public class ViewerSelectMode extends JPanel {
 		JPanel panel = new JPanel(new BorderLayout());
 		panel.setPreferredSize(new Dimension(1200, 200));
 		Font titlefont = new Font("SansSerif", Font.PLAIN, 125);
-		Font btnfont = new Font("SansSerif", Font.BOLD, 30);
 		panel.setBackground(Color.DARK_GRAY);
 		lblHeader.setFont(titlefont);
 		lblHeader.setForeground(Color.WHITE);
-		btnBack.setPreferredSize(new Dimension(200, 100));
-		btnBack.setFont(btnfont);
-		btnBack.setForeground(Color.WHITE);
-		btnBack.setBorderPainted(false);
-		
-		panel.add(btnBack, BorderLayout.WEST);
+
 		panel.add(lblHeader, BorderLayout.CENTER);
 		
 		return panel;
+	}
+	
+	private JPanel buttonPanel() {
+		JPanel bPanel = new JPanel(null);
+		bPanel.setBackground(Color.DARK_GRAY);
+		btnSingle.setBounds(450, 50, 300, 100);
+		btnMulti.setBounds(450, 200, 300, 100);
+			
+		btnSingle.setFont(new Font ("Sans Serif", Font.PLAIN, 30));
+		btnMulti.setFont(new Font ("Sans Serif", Font.PLAIN, 30));
 		
+		bPanel.add(btnSingle);
+		bPanel.add(btnMulti);
+		
+		return bPanel;
 	}
 	
 	private class ButtonListener implements ActionListener {
