434b23164ca41e6e259e143b597b67ab858ad9ae
Elina Kock <elina.kock@gmail.com>
2018-03-20 12:04:37 +0100

    cardlayout pls work

--- src/client/StartGame.java
+++ src/client/StartGame.java
@@ -7,7 +7,8 @@ import javax.swing.*;
 public class StartGame extends JFrame implements ContinueListener {
 	private JPanel cards = new JPanel(new CardLayout());
 	CardLayout cardLayout = (CardLayout)cards.getLayout();
-	
+	private String currentCard = "cardMode";
+
 	public StartGame() {
 		setSize(new Dimension(1200, 800));
 		setTitle("Hangman");
@@ -16,28 +17,28 @@ public class StartGame extends JFrame implements ContinueListener {
 		getContentPane().add(cards);
 		setVisible(true);
 	}
-	
+
 	private void setupCards() {
 		ViewerSelectMode selectMode = new ViewerSelectMode();
 		selectMode.addListener(this);
 		JPanel cardMode = new JPanel();
 		cardMode.add(selectMode);
-		
+
 		ViewerSelectDifficulty selectDifficulty = new ViewerSelectDifficulty();
 		selectMode.addListener(this);
 		JPanel cardDifficulty = new JPanel();
 		cardDifficulty.add(selectDifficulty);
-		
+
 		ViewerSelectCategory selectCategory = new ViewerSelectCategory();
 		selectMode.addListener(this);
 		JPanel cardCategory = new JPanel();
 		cardCategory.add(selectCategory);
-		
+
 		ViewerGame game = new ViewerGame();
 		selectMode.addListener(this);
 		JPanel cardGame = new JPanel();
 		cardGame.add(game);
-		
+
 		cards.add(cardMode, "cardMode");
 		cards.add(cardDifficulty, "cardDifficulty");
 		cards.add(cardCategory, "cardCategory");
@@ -46,14 +47,34 @@ public class StartGame extends JFrame implements ContinueListener {
 
 	@Override
 	public void nextPanel() {
-		cardLayout.next(cards);
+		switch (currentCard) {
+		case "cardMode" : 
+			cardLayout.show(cards, "cardDifficulty");
+			currentCard = "cardDifficulty";
+			break;
+		case "cardDifficulty" : 
+			cardLayout.show(cards, "cardCategory");
+			currentCard = "cardCategory";
+			break;
+		case "cardCategory" : 
+			cardLayout.show(cards, "cardGame");
+			currentCard = "cardGame";
+			break;
+		}
 	}
 
 	@Override
 	public void goBack() {
-		cardLayout.show(cards, "cardMode");
+		switch (currentCard) {
+		case "cardGame" : cardLayout.show(cards, "cardCategory");
+		break;
+		case "cardCategory" : cardLayout.show(cards, "cardDifficulty");
+		break;
+		case "cardDifficulty" : cardLayout.show(cards, "cardMode");
+		break;
+		}
 	}
-	
+
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
