e51f1dbf00656e744efa8db0d652554a421afed8
Jakob Kennerberg <jakobkennerberg98@gmail.com>
2018-03-21 12:08:46 +0100

    Hej

--- src/client/ViewerSelectDifficulty.java
+++ src/client/ViewerSelectDifficulty.java
@@ -7,7 +7,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import java.util.LinkedList;
 import java.util.Random;
 import javax.swing.*;
 
