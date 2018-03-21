98903a610752c994edf4df8f50cbcf677e7c363d
Elina Kock <elina.kock@gmail.com>
2018-03-21 11:11:21 +0100

    Tog bort yuns filer

---
+++ src/client/ViewerMultiplayer.java
@@ -0,0 +1,11 @@
+package client;
+import javax.swing.*; 
+
+/**
+ * Lets the player choose another online player from a list
+ * so that they can play together.
+ *
+ */
+public class ViewerMultiplayer extends JPanel {
+
+}
