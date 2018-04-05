21cbb3396f4c528212e826206f5a7792a0e88bfd
Yun <yunfah.chow@gmail.com>
2018-04-05 08:33:20 +0200

    hej

--- src/client/PlaySound.java
+++ src/client/PlaySound.java
@@ -6,22 +6,27 @@ import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 
 import java.io.File;
+import java.nio.file.Paths;
 
 
 public class PlaySound extends Application {
 
-    public static void main(String[] args) {
-        launch(args);
-       // https://stackoverflow.com/questions/22490064/how-to-use-javafx-mediaplayer-correctly
 
-    }
-
-    @Override
-    public void start(Stage primaryStage) {
-           Media hit = new Media(new File("files/elevator.mp3").toURI().toString());
-           MediaPlayer mediaPlayer = new MediaPlayer(hit);
-           mediaPlayer.play();
+	// https://stackoverflow.com/questions/22490064/how-to-use-javafx-mediaplayer-correctly
 
 
-    }
+
+	@Override
+	public void start(Stage primaryStage) {
+		Media hit = new Media(new File("files/elevator.mp3").toURI().toString());
+		//Media song = new Media(Paths.get("files/elevator.mp3").toUri().toString());
+		MediaPlayer mediaPlayer = new MediaPlayer(hit);
+		mediaPlayer.play();
+		System.out.println(System.getProperty("files/elevator.mp3"));
+
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
