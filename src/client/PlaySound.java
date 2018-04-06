7c22a5b4395a0013966378c55330c8b283a7946f
Yamma <yamma.sarwari@gmail.com>
2018-04-06 11:34:55 +0200

    fixat lite sound tast mannens MED DUELS!

--- src/client/PlaySound.java
+++ src/client/PlaySound.java
@@ -3,15 +3,40 @@ package client;
 import java.applet.*;
 import java.io.File;
 import java.net.*;
+import java.util.Timer;
 
-public class PlaySound {
+public class PlaySound extends Thread {
 	private AudioClip sound;
 
-	public void playElevator() {
+	public void playElevatorLoop() {
 		try{
 			File file = new File("/Users/yammasarwari/git/Repo/Repo/files/elevator.wav");
 			sound =  Applet.newAudioClip(file.toURI().toURL());
 		} catch(MalformedURLException e) {}
-		sound.play();
+		sound.loop();
+	}
+	public void playDuel() {
+		try{
+		File file = new File("/Users/yammasarwari/git/Repo/Repo/files/duel.wav");
+		sound =  Applet.newAudioClip(file.toURI().toURL());
+	} catch(MalformedURLException e) {}
+	sound.loop();
+
+	}
+	public void stopLoop() {
+		sound.stop();
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+		PlaySound t1 = new PlaySound();
+		t1.playElevatorLoop();
+		Thread.sleep(10000);
+		t1.stopLoop();
+		Thread.sleep(5000);
+		t1.playDuel();
+		Thread.sleep(5000);
+		t1.stopLoop();
+
+
 	}
 }
