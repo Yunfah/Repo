a5b6212b2edc2c61ac07e1b0668df202e7f7d618
Yamma <yamma.sarwari@gmail.com>
2018-04-06 11:15:44 +0200

    fixat lite sound tast mannens

--- src/client/PlaySound.java
+++ src/client/PlaySound.java
@@ -1,32 +1,17 @@
 package client;
 
-import javafx.application.Application;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
-import javafx.stage.Stage;
-
+import java.applet.*;
 import java.io.File;
-import java.nio.file.Paths;
+import java.net.*;
 
+public class PlaySound {
+	private AudioClip sound;
 
-public class PlaySound extends Application {
-
-
-	// https://stackoverflow.com/questions/22490064/how-to-use-javafx-mediaplayer-correctly
-
-
-
-	@Override
-	public void start(Stage primaryStage) {
-		Media hit = new Media(new File("files/elevator.mp3").toURI().toString());
-		//Media song = new Media(Paths.get("files/elevator.mp3").toUri().toString());
-		MediaPlayer mediaPlayer = new MediaPlayer(hit);
-		mediaPlayer.play();
-		System.out.println(System.getProperty("files/elevator.mp3"));
-
-	}
-
-	public static void main(String[] args) {
-		launch(args);
+	public void playElevator() {
+		try{
+			File file = new File("/Users/yammasarwari/git/Repo/Repo/files/elevator.wav");
+			sound =  Applet.newAudioClip(file.toURI().toURL());
+		} catch(MalformedURLException e) {}
+		sound.play();
 	}
 }
