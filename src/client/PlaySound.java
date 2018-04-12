c9c3bdc796d6278198546af4b042f947e54e0ab6
Yamma <yamma.sarwari@gmail.com>
2018-04-12 12:49:55 +0200

    ändrat lite med sound, inte fixat än

--- src/client/PlaySound.java
+++ src/client/PlaySound.java
@@ -5,41 +5,60 @@ import java.io.File;
 import java.net.*;
 import java.util.Timer;
 
-public class PlaySound extends Thread {
+public class PlaySound {
 	private AudioClip sound;
+	private AudioClip pop;
+	private AudioClip lose;
+	private AudioClip win;
+	private boolean tast;
 
 	public void playElevatorLoop() {
-		sound = null;
 		try{
 			File file = new File("files/elevator.wav");
 			sound =  Applet.newAudioClip(file.toURI().toURL());
-		} catch(MalformedURLException e) {}
+		} catch (MalformedURLException e) {}
 		sound.loop();
 	}
-	public void playDuel() {
-		sound = null;
-		try{
-		File file = new File("files/duel.wav");
-		sound =  Applet.newAudioClip(file.toURI().toURL());
-	} catch(MalformedURLException e) {}
-	sound.play();
+//
+//	public void lose() {
+//		try {
+//			File file = new File("files/lose.wav");
+//			lose = Applet.newAudioClip(file.toURI().toURL());
+//		} catch (MalformedURLException e) {}
+//		lose.play();
+//		}
+//
+//	public void win() {
+//		try {
+//			File file = new File("files/win.wav");
+//			win = Applet.newAudioClip(file.toURI().toURL());
+//		} catch (MalformedURLException e) {}
+//		win.play();
+//	}
 
-	}
-	public void resumeElevatorLoop() {
-		try {
-			Thread.sleep(7000);
-			playElevatorLoop();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
-	public void stopLoop() {
-		sound.stop();
-	}
+//	public void pop() {
+//		if (!tast) {
+//			try {
+//				File file = new File("files/pop.wav");
+//				pop = Applet.newAudioClip(file.toURI().toURL());
+//			} catch (MalformedURLException e) {
+//			}
+//			tast = true;
+//			pop.play();
+//		} else {
+//			pop.play();
+//		}
+//	}
+//
+//	public void stopLoop() {
+//		sound.stop();
+//	}
 
 	public static void main(String[] args) {
 		PlaySound t1 = new PlaySound();
-		t1.playElevatorLoop();
+
+
+
 
 
 	}
