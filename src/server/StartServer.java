5e7e46b3bd285fc55571789c2af670d849ac5829
Jakob Kennerberg <jakobkennerberg98@gmail.com>
2018-04-26 19:27:28 +0200

    igen

--- src/server/StartServer.java
+++ src/server/StartServer.java
@@ -1,13 +1,9 @@
 package server;
 
-/**
- * Class to start the server via the server UI.
- * @author Elina Kock
- *
- */
 public class StartServer {
 	
 	public static void main(String[] args) {
+		
 		ServerUI ui = new ServerUI();
 	}
 }
